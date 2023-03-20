55b715c4230728ac0cee906252fbf77f1378568f
Emmatsoka <emmatsoka.qwq@gmail.com>
2023-03-20 20:22:03 +0100

    .

---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function kilences(){
+    document.getElementById("kiviteli").value += "9";
+}
+function nyolcas(){
+    document.getElementById("kiviteli").value += "8";
+}
+function hetes(){
+    document.getElementById("kiviteli").value += "7";
+}
+function hatos(){
+    document.getElementById("kiviteli").value += "6";
+}
+function otos(){
+    document.getElementById("kiviteli").value += "5";
+}
+function negyes(){
+    document.getElementById("kiviteli").value += "4";
+}
+function harmas(){
+    document.getElementById("kiviteli").value += "3";
+}
+function kettes(){
+    document.getElementById("kiviteli").value += "2";
+}
+function egyes(){
+    document.getElementById("kiviteli").value += "1";
+}
+function nullas(){
+    document.getElementById("kiviteli").value += "0";
+}
+
+function pluusz(){
+    document.getElementById("kiviteli").value += ' + ';
+
+}
+function minuusz(){
+    document.getElementById("kiviteli").value += ' - ';
+
+}
+function szorzaas(){
+    document.getElementById("kiviteli").value += ' * ';
+
+}
+function osztaas(){
+    document.getElementById("kiviteli").value += ' / ';
+
+}*/
